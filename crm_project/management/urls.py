6637d15ae24feaf532e38e10521938611f935999
jeremiehvt <jeremiehvt@gmail.com>
2019-11-08 17:26:12 +0100

    change view function to view class

--- crm_project/management/urls.py
+++ crm_project/management/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from . import views
-from .views import ResourceList
+from .views import ResourceList, IndexView
 
 # setting app name to use namsepace
 app_name='crm'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', IndexView.as_view(), name='index'),
     path('resources', ResourceList.as_view(), name='allResources'),
     path('missions', views.allMissions, name='allMissions'),
 ]
--- crm_project/management/views.py
+++ crm_project/management/views.py
@@ -1,14 +1,48 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, HttpRequest
-from django.views.generic import ListView
+from django.views.generic import ListView, TemplateView, DetailView
 import requests
-from .models import UserManage, Resource
+from .models import UserManage, Resource, Mission, Client, Company
 
 class ResourceList(ListView):
+    http_method_names = ['get','head','option','trace']
     model = Resource
     template_name = 'management/resource_list.html'
     context_object_name = 'resources'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['meteo'] = "voici la météo"
+
+class IndexView(TemplateView):
+    http_method_names = ['get','head','option','trace']
+    template_name = 'management/index_view.html'
+    context_object_name = 'homepage_infos'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['meteo'] = "voici la météo"
+
+class MissionList(ListView):
+    http_method_names = ['get','head','option','trace']
+    model = Mission
+    template_name = 'management/mission_list.html'
+    context_object_name = 'missions'
+
+class ClientList(ListView):
+    http_method_names = ['get','head','option','trace']
+    model = Mission
+    template_name = 'management/client_list.html'
+    context_object_name = 'clients'
+
+class MissionDetail(DetailView):
+    pass
+
+class ResourceDetail(DetailView):
+    pass
+
+class ClientDetail(DetailView):
+    pass
 
 def index(request):
     return HttpResponse("voici la première page du crm en version basic html bienvenue")
